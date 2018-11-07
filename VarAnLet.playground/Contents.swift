4b4775bfbca6dfeef34421d8f2d655a4e40bc35e
masterUNG <phrombutr@gmail.com>
2018-11-07 10:38:17 +0700

    Operating OK

--- VarAnLet.playground/Contents.swift
+++ VarAnLet.playground/Contents.swift
@@ -15,9 +15,20 @@ var status2Bool: Bool = false
 
 //Constant การประกาศค่าคงที่
 let surnameString: String = "Phrombute"
+let numberString: String = "100"
+
 
 name2String = "MasterUNG1234"
 //surnameString = "AAAA" จะ Error เพราะเป็น let
+
+//Operating
+
+let answer1Int: Int = number1Int + number2Int
+let answer2Int: Int = number1Int + Int(number2Dou)
+let answer3Int: Int = number1Int + Int(numberString)!
+
+let answer4Dou: Double = number2Dou + Double(number1Int)
+
 
 
 
