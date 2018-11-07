20866f83c3cdba3e5b8650898a78f62d105d0837
masterUNG <phrombutr@gmail.com>
2018-11-07 14:26:21 +0700

    Dictionary

--- ArrayAnDictionary.playground/Contents.swift
+++ ArrayAnDictionary.playground/Contents.swift
@@ -4,4 +4,38 @@ import UIKit
 var numberStrins = [11,22,33,44,55]
 var nameStrins = ["AAA", "BBB", "CCC", "DDD"]
 
+print("Hello Console")
+print("numberString at 2 ==> \(numberStrins[2])")
+print("nameStrings[3] ==> \(nameStrins[3])")
+
+print("ขนาดของ Array numberStrings ==> \(numberStrins.count)")
+
+//การเพิ่ม สมาชิคบน Array
+numberStrins.append(123)
+nameStrins.append("Master UNG")
+
+print("number \(numberStrins)")
+numberStrins.remove(at: 2)
+print("number \(numberStrins)")
+
+
+//Dictionary
+var friendDicStrings = ["key1":"Doramon", "key2":"Nopita"]
+var numberDicInt = ["k1":11, "k2":22, "k3":33]
+
+print("friendDicStrings ที่มีคีย์ ว่า key2 ==> \(friendDicStrings["key2"]!)")
+
+print("ขนาดของDic \(numberDicInt.count)")
+
+friendDicStrings["newKey"] = "Master UNG"
+print("friendDicStrings ==> \(friendDicStrings)")
+
+friendDicStrings.removeValue(forKey: "key1")
+print("friendDicStrings ==> \(friendDicStrings)")
+
+
+
+
+
+
 
